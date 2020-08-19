6871a191155141522279412992ea4976d1c821ed
Erica Schimmel <ericaschimmel01@gmail.com>
2020-08-19 03:04:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 // Code your solution here!
 
+function printString(text) {
+  console.log(text[0]);
+  
+  if (text.length > 1) {
+    let newString = text.substring(0, text.length);
+    
+    printString(newString);
+  }
+}
